527a174e6748953a08399e3865a06d78dae33a33
Almas Akhmetov <awakenedduck@gmail.com>
2023-10-15 16:40:14 +0600

    slider works

--- src/Pages/Home/Components/Trending/Trending.jsx
+++ src/Pages/Home/Components/Trending/Trending.jsx
@@ -1,18 +1,42 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react"
 import Movie from "../../../../Components/Movie/Movie"
 import { AiOutlineRight } from "react-icons/ai"
 import { AiOutlineLeft } from "react-icons/ai"
 function Trending({ moviesTrending }) {
+  const [translate, setTranslate] = useState(0)
+  const [imageView, setImageView] = useState(0)
+
+  const trendingStyle = {
+    width: `${moviesTrending.length * 31}rem`,
+    transform: `translateX(-${translate}rem)`,
+    transition: `0.3s ease-in-out`,
+  }
+
+  function handleLeft() {
+    console.log(imageView)
+    if (imageView === 0) return
+    setImageView((prev) => prev - 1)
+    setTranslate((prev) => prev - 31)
+  }
+
+  function handleRight() {
+    console.log(imageView)
+    if (imageView === moviesTrending.length - 3) return
+    setImageView((prev) => prev + 1)
+    setTranslate((prev) => prev + 31)
+  }
+
   return (
     <div className="trending">
       <h1>Trending</h1>
-      <div className="trending__movies">
+      <div className="trending__movies" style={trendingStyle}>
         {moviesTrending.map((trending) => (
           <Movie movie={trending} key={trending.title} trending={true} />
         ))}
       </div>
-      <AiOutlineLeft className="leftArr" />
-      <AiOutlineRight className="rightArr" />
+      <AiOutlineLeft className="leftArr" onClick={handleLeft} />
+      <AiOutlineRight className="rightArr" onClick={handleRight} />
     </div>
   )
 }
